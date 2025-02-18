631823eb37ff03a423c69c69189547107683f28b
rosyimuth <rosyimuth@gmail.com>
2025-02-18 12:59:18 +0700

    Update commit

--- resources/views/livewire/post/index.blade.php
+++ resources/views/livewire/post/index.blade.php
@@ -61,9 +61,10 @@
                             <td class="border px-4 py-2">{{ $posts->firstItem() + $loop->index }}</td>
                             <td class="border px-4 py-2">{{ $post->title }}</td>
                             <td class="border px-4 py-2">{{ $post->description }}</td>
-                            <td class="border px-4 py-2 flex justify-center items-center">
+                            <td class="border px-4 py-2">
+                            <div class="flex justify-center items-center">
                                 @if($post->image)
-                                    <img src="{{ asset('storage/' . $post->image) }}" alt="Uploaded Image" class="max-w-[120px] max-h-[120px] rounded-md object-contain">
+                                    <img src="{{ asset('storage/' . $post->image) }}" alt="Uploaded Image" class="max-w-[100px] max-h-[100px] rounded-md object-contain">
                                 @else
                                     <span>No image</span>
                                 @endif
@@ -77,6 +78,7 @@
                                 <button wire:click.prevent="delete({{ $post->id }})" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
                                     <i class="bi bi-trash"></i> Delete
                                 </button>
+                            </div>
                             </td>
                         </tr>
                     @endforeach
